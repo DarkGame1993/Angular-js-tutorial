63038294fbfe969207eb8461eaf59d4273480741
Varerii Skorokhod <valeriiskorokhod@gmail.com>
2021-09-22 13:13:52 +0300

    pipe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,21 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  // toggle:any = false;
-
-  // currentStyles = {     
-  //   'backgraund':  this.backgroundToggle  ? 'red' : 'blue'  
-  // };
-
-  objs = [
-    {title: "Asus 10", price: "10$", skills: [
-      {color: "red"},
-      {usb: "3.0"}
-    ]},
-  {title: "Iphone 10", price: "100$", skills: [
-      {color: "gold"},
-      {usb: "4.0"}
-    ]
-  },
-]
+ now: Date = new Date();
 }
