85f14d29f772ff205ec1baaa9ca1af7ccda79fa2
Varerii Skorokhod <valeriiskorokhod@gmail.com>
2021-09-22 12:56:39 +0300

    ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,9 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  toggle:any = false;
+  // toggle:any = false;
 
   // currentStyles = {     
   //   'backgraund':  this.backgroundToggle  ? 'red' : 'blue'  
   // };
+
+  objs = [
+    {title: "Asus 10", price: "10$", skills: [
+      {color: "red"},
+      {usb: "3.0"}
+    ]},
+  {title: "Iphone 10", price: "100$", skills: [
+      {color: "gold"},
+      {usb: "4.0"}
+    ]
+  },
+]
 }
