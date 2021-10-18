02f09e372f61476546d3c9b9615a01a84ebc317f
Varerii Skorokhod <valeriiskorokhod@gmail.com>
2021-10-18 17:19:05 +0300

    Async Pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
 
 export interface Post {
   title: string;
@@ -11,17 +12,27 @@ export interface Post {
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  promise: Promise<string> = new Promise<string>(resolve => {
+    setTimeout( () => {
+      resolve('Promise resolved')
+    }, 3000)
+  });
 
-  search = ""
-  searchField = "title"
+  date:Observable<Date> = new Observable( obs => {
+    setInterval (() => {
+      obs.next(new Date())
+    }, 1000)
+  })
+  // search = ""
+  // searchField = "title"
 
-  posts:Post[] = [
-    {title: 'Beer', text: 'Самое лучшее пиво в мире'},
-    {title: 'Bread', text: 'The best Bread in the world'},
-    {title: 'JavaScript', text: 'The best lang in framework'}
-  ]
+  // posts:Post[] = [
+  //   {title: 'Beer', text: 'Самое лучшее пиво в мире'},
+  //   {title: 'Bread', text: 'The best Bread in the world'},
+  //   {title: 'JavaScript', text: 'The best lang in framework'}
+  // ]
 
-  AddPost() {
-    this.posts.push({title: 'Angular 8', text: 'Work angular top'})
-  }
+  // AddPost() {
+  //   this.posts.push({title: 'Angular 8', text: 'Work angular top'})
+  // }
 }
