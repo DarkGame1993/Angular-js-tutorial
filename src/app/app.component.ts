cbd57cb8fd5f09aed609fc78a99824727dadd510
Varerii Skorokhod <valeriiskorokhod@gmail.com>
2021-10-19 17:31:01 +0300

    Added  services (global, local)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,37 +1,18 @@
-import { Component, OnInit } from '@angular/core';
-
-export interface Post {
-  title: string;
-  text: string;
-  id?:number;
-}
+import { Component} from '@angular/core';
+import { AppCounterService } from './services/app-counter.service';
+import { LocalCounterService } from './services/local-counter.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  providers: [LocalCounterService]
 })
-export class AppComponent implements OnInit {
- posts: Post[] = [
-   {title: 'Хочу выучить Ангуляр компоненты', text: 'Я все еще учу компоненты', id: 1},
-   {title: 'Следуюший блок ', text: 'Будет про директивы и еще про пайпы', id: 2}
-  ];
-  ngOnInit() :void {
-    setTimeout(() => {
-      console.log('TimeOut')
-      this.posts[0] = {
-        title: 'changed',
-        text: ' changed 2',
-        id: 33
-      }
-    })
-  }
-  updatePosts(post: Post) {
-    this.posts.unshift(post);
-    // console.log('Post', post)
-  }
 
-  removePost(id: number) {
-    this.posts = this.posts.filter (p => p.id !== id)
-  }
+export class AppComponent {
+ constructor(
+   public appCounterSЦЦervice: AppCounterService,
+   private localCounterService: LocalCounterService
+   ) {}
+
 }
