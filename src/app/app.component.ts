9a3ba1bdc1caadc7d659c4e17697487ba2895f71
Varerii Skorokhod <valeriiskorokhod@gmail.com>
2021-10-30 16:10:04 +0300

    Method post

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { title } from 'process';
 
 export interface Todo {
   title: string,
@@ -14,13 +15,30 @@ export interface Todo {
 export class AppComponent implements OnInit {
   constructor(private http: HttpClient) {}
   
-todos: Todo[] = [];
-
+  todos: Todo[] = [];
+  todoTittle = ''
   ngOnInit () {
     this.http.get<Todo[]>('https://jsonplaceholder.typicode.com/todos?_limit=2')
       .subscribe(todos => {
         console.log('This response', todos)
         this.todos = todos
       })
+  }
+
+  addTodo () {
+    if(!this.todoTittle.trim()) {
+      return
+    } 
+
+    const newTodo: Todo = {
+      title: this.todoTittle,
+      completed: false
+    }
+    this.http.post<Todo>('https://jsonplaceholder.typicode.com/todos', newTodo)
+    .subscribe(todo => {
+      console.log(todo, 'todo')
+      this.todos.push(todo)
+      this.todoTittle = ''
+    })
   }
 }
