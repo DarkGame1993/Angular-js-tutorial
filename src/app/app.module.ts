7c09dd5bf978cfe724c0dc2d14583bbbaabc07d5
Varerii Skorokhod <valeriiskorokhod@gmail.com>
2021-11-11 12:46:18 +0200

    HttpInterceptor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, Provider } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AppComponent } from './app.component';
 import { FormsModule} from '@angular/forms';
-import { HttpClientModule }   from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS }   from '@angular/common/http';
+import { AuthInterceptor } from './auth.interceptor';
 
+const INTERCEPTOR_PROVIDERS: Provider = {
+  provide: HTTP_INTERCEPTORS,
+  useClass: AuthInterceptor,
+  multi: true
+}
 
 @NgModule({
   declarations: [
@@ -17,7 +23,7 @@ import { HttpClientModule }   from '@angular/common/http';
     CommonModule,
     HttpClientModule
   ],
-  providers: [],
+  providers: [INTERCEPTOR_PROVIDERS],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
