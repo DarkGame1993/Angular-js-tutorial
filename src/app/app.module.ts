a9d69c02d9ea5a8f4e039a2e20e76a028420f29c
Varerii Skorokhod <valeriiskorokhod@gmail.com>
2021-11-18 21:18:22 +0200

    Registred routs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+ 
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
-import { PostFormComponent } from './post-form/post-form.component';
-import { PostComponent } from './post/post.component';
+import { AboutComponent } from './about/about.component'
+import { HomeComponent } from './home/home.component'
+import { PostsComponent } from './posts/posts.component'
+import { PostComponent } from './post/post.component'
+import { AboutExtraComponent } from './about-extra/about-extra.component'
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PostFormComponent,
-    PostComponent
+    AboutComponent,
+    HomeComponent,
+    PostsComponent,
+    PostComponent,
+    AboutExtraComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    CommonModule
+    CommonModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -1,63 +1,10 @@
-import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, ChangeDetectionStrategy, Component, ContentChild, DoCheck, ElementRef, EventEmitter, Input, OnChanges, OnDestroy, OnInit, Output, SimpleChanges, ViewEncapsulation } from '@angular/core';
-import { Post } from '../app.component';
+import {Component} from '@angular/core'
 
 @Component({
   selector: 'app-post',
   templateUrl: './post.component.html',
-  styleUrls: ['./post.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush,
-  encapsulation: ViewEncapsulation.None,
+  styleUrls: ['./post.component.scss']
 })
-export class PostComponent implements 
-  OnInit, 
-  OnChanges,
-  DoCheck,
-  AfterContentInit,
-  AfterContentChecked,
-  AfterViewInit,
-  AfterViewChecked,
-  OnDestroy
+export class PostComponent {
 
-{
-
-  @Input() post: Post
-  @Output() onRemove = new EventEmitter<Number>();
-  @ContentChild ('info', {static: true}) infoRef: ElementRef
-
-  removePost() {
-    this.onRemove.emit(this.post.id)
-  }
-
-  ngOnChanges(changes: SimpleChanges): void {
-    console.log('ngOnChanges', changes);
-  }
-
-  ngOnInit(): void {
-    console.log('ngOnInit')
-    // console.log(this.infoRef.nativeElement)
-  }
-
-  ngDoCheck(): void {
-    console.log('ngDoCheck')
-  }
-  
-  ngAfterContentInit(): void {
-    console.log('ngAfterContentInit')
-  }
-
-  ngAfterContentChecked(): void {
-    console.log('ngAfterContentChecked')
-  }
-
-  ngAfterViewInit(): void{
-    console.log('ngAfterViewInit')
-  }
-
-  ngAfterViewChecked(): void {
-    console.log('ngAfterViewChecked')
-  }
-
-  ngOnDestroy(): void {
-    console.log('ngOnDestroy')
-  }
 }
