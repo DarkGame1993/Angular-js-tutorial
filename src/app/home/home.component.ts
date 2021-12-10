98850e559338ef50f3bddf2c03dd6db088b29661
Varerii Skorokhod <valeriiskorokhod@gmail.com>
2021-12-10 12:47:35 +0200

    Params

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,7 +7,9 @@ import { ActivatedRoute, Router } from '@angular/router';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
-  constructor(private route:ActivatedRoute,private router:Router) { }
+  constructor(
+    private route:ActivatedRoute,
+    private router:Router) { }
 
   goToPostPage() {
     this.router.navigate(['posts'])
