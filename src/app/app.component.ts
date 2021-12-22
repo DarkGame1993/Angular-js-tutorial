3a285bcb95ddc31898fd6699f8ee8bbc72d5da51
Varerii Skorokhod <valeriiskorokhod@gmail.com>
2021-12-22 17:03:14 +0200

    Moduls start

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,37 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-
-export interface Post {
-  title: string;
-  text: string;
-  id?:number;
-}
+import {Component} from '@angular/core'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
- posts: Post[] = [
-   {title: 'Хочу выучить Ангуляр компоненты', text: 'Я все еще учу компоненты', id: 1},
-   {title: 'Следуюший блок ', text: 'Будет про директивы и еще про пайпы', id: 2}
-  ];
-  ngOnInit() :void {
-    setTimeout(() => {
-      console.log('TimeOut')
-      this.posts[0] = {
-        title: 'changed',
-        text: ' changed 2',
-        id: 33
-      }
-    })
-  }
-  updatePosts(post: Post) {
-    this.posts.unshift(post);
-    // console.log('Post', post)
-  }
-
-  removePost(id: number) {
-    this.posts = this.posts.filter (p => p.id !== id)
-  }
+export class AppComponent {
+  constructor() {}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,19 +4,27 @@ import { CommonModule } from '@angular/common';
 
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
-import { PostFormComponent } from './post-form/post-form.component';
-import { PostComponent } from './post/post.component';
+import {HomePageComponent} from './home-page/home-page.component'
+import {AppRoutingModule} from './app-routing.module'
+import {AboutPageComponent} from './about-page/about-page.component'
+import {AboutExtraPageComponent} from './about-page/about-extra-page/about-extra-page.component'
+import {ColorDirective} from './shared/color.directive'
+import {PageNamePipe} from './shared/page-name.pipe'
 
 @NgModule({
   declarations: [
     AppComponent,
-    PostFormComponent,
-    PostComponent
+    HomePageComponent,
+    AboutPageComponent,
+    AboutExtraPageComponent,
+    ColorDirective,
+    PageNamePipe
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    CommonModule
+    CommonModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
